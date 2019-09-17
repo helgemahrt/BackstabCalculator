bac6fb1b5764c39b74dd3092f161f974f78b6f47
Helge Mahrt <hmahrt@microsoft.com>
2019-09-17 13:58:33 +0200

    Improved command output

--- core.lua
+++ core.lua
@@ -1,86 +1,279 @@
+-- lookups
+local rank_names =
+{
+    [ 1]=1,[ 2]= 2,[ 3]= 3,[ 4]= 4,[ 5]= 5,[ 6]= 6,[ 7]= 7,[ 8]=8,
+    [ 9]=9,[10]=10,[11]=11,[12]=12,[13]=13,[14]=14,[15]=15,[16]=16,
+
+    ["Rank 1" ]= 1,["Rank 2" ]= 2,["Rank 3" ]= 3,["Rank 4" ]= 4,["Rank 5" ]=5,
+    ["Rank 6" ]= 6,["Rank 7" ]= 7,["Rank 8" ]= 8,["Rank 9" ]= 9,["Rank 11"]=10,
+    ["Rank 12"]=11,["Rank 13"]=12,["Rank 14"]=13,["Rank 15"]=15,["Rank 16"]=16,
+};
+local BS_BONUS = { 15, 30, 48, 69, 90, 135, 165, 210 };
+local AB_BONUS = { 70, 100, 125, 185, 230, 290 };
+local SS_BONUS = { 3, 6, 10, 15, 22, 33, 52, 68 };
+
+function nextarg(msg, pattern)
+    if ( not msg or not pattern ) then
+        return nil, nil;
+    end;
+    local s,e = string.find(msg, pattern);
+    if ( s ) then
+        local word = strsub(msg, s, e);
+        msg = strsub(msg, e+1);
+        return word, msg;
+    end;
+    return nil, msg;
+end;
+
+function GetWeaponType(weaponLink)
+    local _, _, _, _, _, itemType, itemSubType, _, _, _, _ = GetItemInfo(weaponLink);
+    if (itemType == "Weapon")
+    then
+        return itemSubType;
+    else
+        return nil;
+    end;
+end;
+
+function IsWeaponOfType(weaponLink, type)
+    local weaponType = GetWeaponType(weaponLink);
+    if weaponType == nil
+    then
+        return false;
+    else
+        return weaponType == type;
+    end;
+end;
+
+function ShowBackstab(weaponLink)
+    return rank_names[GetSpellSubtext("Backstab")] ~= nil and IsWeaponOfType(weaponLink, "Daggers");
+end;
+
+function ShowAmbush(weaponLink)
+    return rank_names[GetSpellSubtext("Ambush")] ~= nil and IsWeaponOfType(weaponLink, "Daggers");
+end;
+
+function ShowSinisterStrike(weaponLink)
+    return rank_names[GetSpellSubtext("Sinister Strike")] ~= nil and IsRogueWeapon(weaponLink);
+end;
+
+function ShowHemorrhage(weaponLink)
+    return false;
+end;
+
+function ShowGhostlyStrike(weaponLink)
+    return false;
+end;
+
+function IsRogueWeapon(weaponLink)
+    local _, _, _, _, _, itemType, itemSubType, _, _, _, _ = GetItemInfo(weaponLink);
+    if (itemType == "Weapon")
+    then
+        return  itemSubType == "One-Handed Swords" or 
+                itemSubType == "One-Handed Maces" or 
+                itemSubType == "Daggers" or 
+                itemSubType == "Fist Weapons";
+    else
+        return false;
+    end;
+end;
+
+function GetWeapon(msg)
+    local cmd;
+    cmd, msg = nextarg(msg, "[%w]+");
+
+    if (not cmd) -- use equipped weapon
+    then
+        return GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot"));
+    else
+        return msg;
+    end;
+end;
+
+function GetAttackPowerOnWeapon(weaponLink)
+    local stats = GetItemStats(weaponLink);
+
+    local agi = stats["ITEM_MOD_AGILITY_SHORT"] or 0;
+    local str = stats["ITEM_MOD_STRENGTH_SHORT"] or 0;
+    local rawAP = stats["ITEM_MOD_ATTACK_POWER_SHORT"] or 0;
+    
+    local strAPonWeapon = GetAttackPowerForStat(2, agi);
+    local agiAPonWeapon = GetAttackPowerForStat(1, str);
+    
+    return strAPonWeapon + agiAPonWeapon + rawAP;
+end;
+
+function GetWeaponMinMaxSpeedDamage(weaponLink)
+    GameTooltip:SetOwner(UIParent,"ANCHOR_NONE");
+    GameTooltip:SetHyperlink(weaponLink);
+    
+    local _, minDmg, maxDmg, speed;
+    for k=1,GameTooltip:NumLines(),1
+    do
+        if (not minDmg and not maxDmg)
+        then
+            local line = _G["GameTooltipTextLeft"..k]:GetText() or "";
+            _, _, minDmg, maxDmg = string.find(line, "(%d+)[^%d]+(%d+)%sDamage");
+        end;
+
+        if (not speed)
+        then
+            local line = _G["GameTooltipTextRight"..k]:GetText() or "";
+            _, _, speed = string.find(line, "Speed%s([%d%.]+)");
+        end;
+
+        if (minDmg and maxDmg and speed)
+        then
+            break;
+        end;
+    end;
+    GameTooltip:Hide();
+
+    if minDmg and maxDmg and speed then
+        return tonumber(minDmg), tonumber(maxDmg), tonumber(speed);
+    else
+        return 0, 0, 1.0;
+    end
+end;
+
+function GetWeaponDamage(weaponLink)
+    local baseAP, posAPBuff, negAPBuff = UnitAttackPower("player");
+
+    local currentWeaponAP = GetAttackPowerOnWeapon(GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot")));
+    local targetWeaponAP = GetAttackPowerOnWeapon(weaponLink);
+
+    local attackPowerWithTargetWeapon = baseAP - currentWeaponAP + targetWeaponAP;
+    local attackPowerBonusDamage = attackPowerWithTargetWeapon / 14;
+
+    local lowDmg, hiDmg, speed = GetWeaponMinMaxSpeedDamage(weaponLink);
+    local weaponDamageBonus = speed * attackPowerBonusDamage;
+
+    return lowDmg + weaponDamageBonus, hiDmg + weaponDamageBonus;
+end;
+
+function round(x)
+    return x + 0.5 - (x + 0.5) % 1
+end;
+
+function GetBackstabDamage(lowDmg, hiDmg, opportunityRank, backStabRank, lethalityRank)
+    local bsMin = lowDmg * (1.5 + 0.04 * opportunityRank) + BS_BONUS[backStabRank];
+    local bsMax = hiDmg  * (1.5 + 0.04 * opportunityRank) + BS_BONUS[backStabRank];
+    local bsCritMin = bsMin * 2 * (1 + 0.06 * lethalityRank);
+    local bsCritMax = bsMax * 2 * (1 + 0.06 * lethalityRank);
+
+    return bsMin, bsMax, bsCritMin, bsCritMax;
+end;
+
+function GetAmbushDamage(lowDmg, hiDmg, opportunityRank, ambushRank)
+    local ambushMin = lowDmg * (2.5 + 0.04 * opportunityRank) + AB_BONUS[ambushRank];
+    local ambushMax = hiDmg  * (2.5 + 0.04 * opportunityRank) + AB_BONUS[ambushRank];
+    local ambushCritMin = ambushMin * 2;
+    local ambushCritMax = ambushMax * 2;
+
+    return ambushMin, ambushMax, ambushCritMin, ambushCritMax;
+end;
+
+function GetSinisterStringDamage(lowDmg, hiDmg, sinisterStrikeRank, aggressionRank, lethalityRank)
+    local ssMin = (lowDmg + SS_BONUS[sinisterStrikeRank]) * (1 + 0.02 * aggressionRank);
+    local ssMax = (hiDmg  + SS_BONUS[sinisterStrikeRank]) * (1 + 0.02 * aggressionRank);
+    local ssCritMin = ssMin * 2 * (1 + 0.06 * lethalityRank);
+    local ssCritMax = ssMax * 2 * (1 + 0.06 * lethalityRank);
+
+    return ssMin, ssMax, ssCritMin, ssCritMax;
+end;
+
+function GetDiffSign(diff)
+    if diff == 0
+    then
+        return ""
+    else if diff > 0
+        then
+            return "|cFF00FF00+";
+        else
+            return "|cffff0000";
+        end;
+    end;
+end;
+
+function PrintSkillDamage(skillName, oldMin, oldMax, oldCritMin, oldCritMax, newMin, newMax, newCritMin, newCritMax)
+    local diffMin = newMin - oldMin;
+    local diffMax = newMax - oldMax;
+    local diffCritMin = newCritMin - oldCritMin;
+    local diffCritMax = newCritMax - oldCritMax;
+
+    print(string.format("%s: %d - %d (%s%d|r - %s%d|r), crit: %d - %d (%s%d|r - %s%d|r)", 
+        skillName, 
+        newMin, newMax,
+        GetDiffSign(diffMin), diffMin,
+        GetDiffSign(diffMax), diffMax,
+        newCritMin, newCritMax,
+        GetDiffSign(diffCritMin), diffCritMin,
+        GetDiffSign(diffCritMax), diffCritMax));
+end;
+
 SLASH_BackstabCalculator1, SLASH_BackstabCalculator2 = '/bsc', '/backstabcalculator';
 SlashCmdList["BackstabCalculator"] = function(msg)
     local _, class, _ = UnitClass("player");    
     if (class == "ROGUE")
     then
-        -- lookups
-        local rank_names =
-        {
-            [ 1]=1,[ 2]= 2,[ 3]= 3,[ 4]= 4,[ 5]= 5,[ 6]= 6,[ 7]= 7,[ 8]=8,
-            [ 9]=9,[10]=10,[11]=11,[12]=12,[13]=13,[14]=14,[15]=15,[16]=16,
-
-            ["Rank 1" ]= 1,["Rank 2" ]= 2,["Rank 3" ]= 3,["Rank 4" ]= 4,["Rank 5" ]=5,
-            ["Rank 6" ]= 6,["Rank 7" ]= 7,["Rank 8" ]= 8,["Rank 9" ]= 9,["Rank 11"]=10,
-            ["Rank 12"]=11,["Rank 13"]=12,["Rank 14"]=13,["Rank 15"]=15,["Rank 16"]=16,
-        };
-        local BS_BONUS = { 15, 30, 48, 69, 90, 135, 165, 210 };
-        local AB_BONUS = { 70, 100, 125, 185, 230, 290 };
-        local SS_BONUS = { 3, 6, 10, 15, 22, 33, 52, 68 };
-
-        -- Min-Max damage on the main hand
-        local lowDmg, hiDmg, _, _, _, _, _ = UnitDamage("player");
-
-        -- points in Opportunity
-        local _, _, _, _, opportunityRank, _, _, _ = GetTalentInfo(3, 2);
-
-        -- points in Improved Ambush
-        local _, _, _, _, impAmbushRank, _, _, _ = GetTalentInfo(3, 8);
-
-        -- points in Improved Backstab
-        local _, _, _, _, impBackstabRank, _, _, _ = GetTalentInfo(2, 4);
-
-        -- points in Lethality
-        local _, _, _, _, lethalityRank, _, _, _ = GetTalentInfo(1, 9);
-
-        -- points in Aggression
-        local _, _, _, _, aggressionRank, _, _, _ = GetTalentInfo(2, 18);
-
-        local backStabRank = rank_names[GetSpellSubtext("Backstab")];
-        local ambushRank = rank_names[GetSpellSubtext("Ambush")];
-        local sinisterStrikeRank = rank_names[GetSpellSubtext("Sinister Strike")];    
-
-        local mainHandLink = GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot"));
-        local _, _, _, _, _, itemType, itemSubType, _, _, _, _ = GetItemInfo(mainHandLink);
-
-        if (itemType == "Weapon")
+        local weaponLink = GetWeapon(msg);
+        if (IsRogueWeapon(weaponLink))
         then
-            if (itemSubType == "Daggers")
-            then
-                -- Backstab damage
-                if (backStabRank ~= nil)
-                then
-                    local bsMin = lowDmg + (1.5 + 0.04 * opportunityRank) + BS_BONUS[backStabRank];
-                    local bsMax = hiDmg  + (1.5 + 0.04 * opportunityRank) + BS_BONUS[backStabRank];
-                    local bsCritMin = bsMin * 2 * (1 + 0.06 * lethalityRank);
-                    local bsCritMax = bsMax * 2 * (1 + 0.06 * lethalityRank);
-                    print("Backstab:", floor(bsMin), "-", floor(bsMax), "crit:", floor(bsCritMin), "-", floor(bsCritMax));
-                else
-                    print("Backstab not learned.")
-                end;
+            print("Damage with:", weaponLink);
 
-                -- Ambush damage
-                if (ambushRank ~= nil) 
-                then
-                    local ambushMin = lowDmg + (2.5 + 0.04 * opportunityRank) + AB_BONUS[ambushRank];
-                    local ambushMax = hiDmg  + (2.5 + 0.04 * opportunityRank) + AB_BONUS[ambushRank];
-                    local ambushCritMin = ambushMin * 2;
-                    local ambushCritMax = ambushMax * 2;
-                    print("Ambush:", floor(ambushMin), "-", floor(ambushMax), "crit:", floor(ambushCritMin), "-", floor(ambushCritMax));
-                else
-                    print("Ambush not learned.")
-                end;
-            else
-                print("No dagger equipped in main hand. Skipping Backstab and Ambush damage.")
+            -- Min-Max damage on the weapon
+            local currentLowDmg, currentHighDmg = GetWeaponDamage(GetInventoryItemLink("player", GetInventorySlotInfo("MainHandSlot")));
+            local targetLowDmg, targetHighDmg = GetWeaponDamage(weaponLink);
+            
+            PrintSkillDamage("White damage", round(currentLowDmg), round(currentHighDmg), round(currentLowDmg * 2), round(currentHighDmg * 2), round(targetLowDmg), round(targetHighDmg), round(targetLowDmg * 2), round(targetHighDmg * 2));
+
+            -- points in Opportunity
+            local _, _, _, _, opportunityRank, _, _, _ = GetTalentInfo(3, 2);
+
+            -- points in Improved Ambush
+            local _, _, _, _, impAmbushRank, _, _, _ = GetTalentInfo(3, 8);
+
+            -- points in Improved Backstab
+            local _, _, _, _, impBackstabRank, _, _, _ = GetTalentInfo(2, 4);
+
+            -- points in Lethality
+            local _, _, _, _, lethalityRank, _, _, _ = GetTalentInfo(1, 9);
+
+            -- points in Aggression
+            local _, _, _, _, aggressionRank, _, _, _ = GetTalentInfo(2, 18);
+
+            local backStabRank = rank_names[GetSpellSubtext("Backstab")];
+            local ambushRank = rank_names[GetSpellSubtext("Ambush")];
+            local sinisterStrikeRank = rank_names[GetSpellSubtext("Sinister Strike")];    
+
+            -- Backstab damage
+            if (ShowBackstab(weaponLink))
+            then
+                local currentBsMin, currentBsMax, currentBsCritMin, currentBsCritMax = GetBackstabDamage(currentLowDmg, currentHighDmg, opportunityRank, backStabRank, lethalityRank);
+                local newBsMin, newBsMax, newBsCritMin, newBsCritMax = GetBackstabDamage(targetLowDmg, targetHighDmg, opportunityRank, backStabRank, lethalityRank);
+
+                PrintSkillDamage("Backstab", currentBsMin, currentBsMax, currentBsCritMin, currentBsCritMax, newBsMin, newBsMax, newBsCritMin, newBsCritMax);
+            end;
+
+            -- Ambush damage
+            if (ShowAmbush(weaponLink)) 
+            then
+                local currentAmbushMin, currentAmbushMax, currentAmbushCritMin, currentAmbushCritMax = GetAmbushDamage(currentLowDmg, currentHighDmg, opportunityRank, ambushRank);
+                local newAmbushMin, newAmbushMax, newAmbushCritMin, newAmbushCritMax = GetAmbushDamage(targetLowDmg, targetHighDmg, opportunityRank, ambushRank);
+
+                PrintSkillDamage("Ambush", currentAmbushMin, currentAmbushMax, currentAmbushCritMin, currentAmbushCritMax, newAmbushMin, newAmbushMax, newAmbushCritMin, newAmbushCritMax);
             end;
 
             -- Sinister Strike damage
-            local ssMin = (lowDmg + SS_BONUS[sinisterStrikeRank]) * (1 + 0.02 * aggressionRank);
-            local ssMax = (hiDmg  + SS_BONUS[sinisterStrikeRank]) * (1 + 0.02 * aggressionRank);
-            local ssCritMin = ssMin * 2 * (1 + 0.06 * lethalityRank);
-            local ssCritMax = ssMax * 2 * (1 + 0.06 * lethalityRank);
-            print("Sinister Strike:", floor(ssMin), "-", floor(ssMax), "crit:", floor(ssCritMin), "-", floor(ssCritMax));
+            if (ShowSinisterStrike(weaponLink))
+            then
+                local currentSsMin, currentSsMax, currentSsCritMin, currentSsCritMax = GetSinisterStringDamage(currentLowDmg, currentHighDmg, sinisterStrikeRank, aggressionRank, lethalityRank);
+                local newSsMin, newSsMax, newSsCritMin, newSsCritMax = GetSinisterStringDamage(targetLowDmg, targetHighDmg, sinisterStrikeRank, aggressionRank, lethalityRank);
+                
+                PrintSkillDamage("Sinister Strike", currentSsMin, currentSsMax, currentSsCritMin, currentSsCritMax, newSsMin, newSsMax, newSsCritMin, newSsCritMax);
+            end;
         else
-            print("No weapon equipped in the main hand.");
+            print("Not a Rogue weapon.");
         end;
     end;
 end;
