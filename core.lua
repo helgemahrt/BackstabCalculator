de94674409c02099cbe30d93f4f008a3cdb32129
Helge Mahrt <hmahrt@microsoft.com>
2019-09-23 15:29:41 +0200

    Added diagnostic message for bug hunting

--- core.lua
+++ core.lua
@@ -31,6 +31,7 @@ end;
 
 function GetWeaponType(weaponLink)
     local _, _, _, _, _, itemType, itemSubType, _, _, _, _ = GetItemInfo(weaponLink);
+    console.log('GetWeaponType', weaponLink, itemType, itemSubType);
     if (itemType == "Weapon")
     then
         return itemSubType;
